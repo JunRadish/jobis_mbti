07d469f91600ca324f95205d7dd8f11deb9712bf
김준민 <junminkim@gimjunmin-ui-MacBookPro.local>
2022-01-05 11:52:18 +0900

    add compress step

--- scripts/backup_source.sh
+++ scripts/backup_source.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Back up previous php file
-rm -rf /data/mbti/backup-*
-mkdir /data/mbti/backup-$(date +%Y%m%d%H%M%S)
-mv /data/mbti/app/* /data/mbti/backup-$(date +%Y%m%d%H%M%S)
+rm -rf /data/mbti/backup/*
+mv /data/mbti/app/* /data/mbti/backup
+tar -zcvf /data/mbti/backup-$(date +%Y%m%d%H%M%S).tar.gz /data/mbti/backup/*
